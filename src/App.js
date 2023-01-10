7575707b14105e77e6b4044e858071ca5b5142af
Swati634 <120095594+Swati634@users.noreply.github.com>
2023-01-10 18:02:02 +0530

    learning react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Covid from './components/covid'
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Covid />
+    </div>
+  )
+}
+
+export default App
+
